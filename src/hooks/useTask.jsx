2e75504f232142fd9fa5b67f06eae242e4386b27
MicheleCasani <casanimichele@gmail.com>
2025-08-11 12:35:41 +0200

    [upd] mileston 8 done

--- src/hooks/useTask.jsx
+++ src/hooks/useTask.jsx
@@ -54,8 +54,28 @@ const useTask = () => {
 
     }
 
-    const removeTask = () => {
-        // Funzione per rimuovere un task
+    const removeTask = async (deleteTask) => {
+        try {
+            const response = await fetch(`${API_URL}/tasks/${deleteTask.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            const data = await response.json();
+            if (data.success) {
+                setTask((prevTask) => {
+                    return prevTask.filter(task => task.id !== deleteTask.id);
+                })
+            }
+            else {
+                throw new Error('Errore durante l\'eliminazione del task:', data.message);
+            }
+        }
+        catch (error) {
+            throw error; // Rilancia l'errore per gestirlo nel componente
+        }
+
     }
 
     const updateTask = () => {
--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalContext'
 
@@ -7,22 +7,30 @@ const TaskDetail = () => {
 
     const { id } = useParams(); // tramite l'hook useparams recuperi l'id del task selezionato
 
-    const { task } = useContext(GlobalContext);
+    const { task, removeTask } = useContext(GlobalContext);
+
+    const navigate = useNavigate();
+
+    const handleDelete = async () => {
+        try {
+            await removeTask(currentTask);
+            alert("Task eliminato con successo!");
+            navigate('/');
+        } catch (error) {
+            alert("Impossibile eliminare il task!");
+        }
+    }
 
     const currentTask = task.find(task => {
         return (
-            task.id == id
+            task.id === parseInt(id)
         )
     })
-    if (!currentTask) return <div>Task non trovato!</div>; // se non trovi il task con quell'id, ritorni un messaggio di errore
+    if (!currentTask) return <h2>Task non trovato!</h2>; // se non trovi il task con quell'id, ritorni un messaggio di errore
 
     return (
         <div className="task-detail">
             <h2>Dettagli Task</h2>
-
-            <div className="task-info">
-                <strong>ID:</strong> {currentTask.id}
-            </div>
 
             <div className="task-info">
                 <strong>Nome:</strong> {currentTask.title}
@@ -40,7 +48,7 @@ const TaskDetail = () => {
                 <strong>Creato il:</strong> {new Date(currentTask.createdAt).toLocaleDateString()}
             </div>
 
-            <button className="delete-button" onClick={() => console.log("Elimino task")}>
+            <button className="delete-button" onClick={handleDelete}>
                 Elimina Task
             </button>
         </div>
